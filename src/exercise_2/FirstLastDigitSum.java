5c302b03bf7d6cadf36152eca3a4d9da6990c0aa
Leyli2020 <74834945+Leyli2020@users.noreply.github.com>
2021-11-13 21:41:26 +0300

    Added exercise FirstLastDigitSum

---
+++ src/exercise_2/FirstLastDigitSum.java
@@ -0,0 +1,16 @@
+package exercise_2;
+
+public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int firstDigit = 0;
+        int lastDigit = number % 10;
+        while (number > 0) {
+            firstDigit = number % 10;
+            number = number / 10;
+        }
+        return firstDigit + lastDigit;
+    }
+}
