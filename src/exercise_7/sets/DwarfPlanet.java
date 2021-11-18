685c0e7e1710a862749d7c201724b6804aef4429
Leyli2020 <74834945+Leyli2020@users.noreply.github.com>
2021-11-18 23:13:34 +0300

    Added exercise Sets

---
+++ src/exercise_7/sets/DwarfPlanet.java
@@ -0,0 +1,8 @@
+package exercise_7.sets;
+
+public class DwarfPlanet extends HeavenlyBody{
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+
+}
---
+++ src/exercise_7/sets/Moon.java
@@ -0,0 +1,7 @@
+package exercise_7.sets;
+
+public class Moon extends HeavenlyBody{
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+}
